630130c379dfcd75fa8570f8d8a07cc752f8c1f2
Alec ORourke <aforourke888@gmail.com>
2023-06-06 16:02:53 -0500

    Feature 7 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.views import View
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        context = {"form": form}
+        return render(request, "accounts/login.html", context)
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("list_projects")
+        context = {"form": form}
+        return render(request, "accounts/login.html", context)
